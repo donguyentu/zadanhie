91f234418ddda3a9a87f26ef87ddb42c11c72bbd
donguyentu <76266308+donguyentu@users.noreply.github.com>
2020-12-25 00:13:19 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,15 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from math import pi
+import os
 
-f = open("C:/results/test.txt", "a")
 x = -5.12
 def y(x): 
   y = 10 + x**2 - 10*np.cos(2*pi*x)
   y1 = np.round(y,2)
   return y1
 
-
+# создание файла
+try:
+  os.mkdir('results')
+except OSError:
+  pass
+complete_file = os.path.join('results', 'task_01_307B_do_1.txt')
+f = open(complete_file, 'w')
+# текстовый файл с таблицей
 f.write("X         Y")
 
 while x <= 5.12: 
@@ -18,7 +25,7 @@ while x <= 5.12:
 
 f.close()
 
-x = np.arange(-5.12, 5.12)
+x = np.arange(-5.12, 5.12,0.01)
 fig, ax = plt.subplots()
 ax.plot(x, y(x))  
 lgnd = ax.legend(['y'], loc='upper center', shadow=True)
